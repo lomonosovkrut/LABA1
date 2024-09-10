6bd067b38d0c239a044aef7bae7eb9c2aff43344
streltsov <167978599+lomonosovkrut@users.noreply.github.com>
2024-09-10 10:37:00 +0300

    Add files via upload

---
+++ laba 1-3/лаба 1 задание 3.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int i, n, * mas;
+    printf("Введите размер массива: ");
+    scanf("%d", &n);
+    srand(time(NULL));
+    mas = (int*)malloc(n * sizeof(int));
+    printf("Сгенерированный массив: ");
+    for (i = 0; i < n; i++) {
+        mas[i] = rand() % 100; 
+        printf("%d ", mas[i]);
+    }
+    free(mas);
+    return(0);
+}
+
+
