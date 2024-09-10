d1efc07e072e3ce1a56c7351037315ddebefcd78
streltsov <167978599+lomonosovkrut@users.noreply.github.com>
2024-09-10 10:36:34 +0300

    Add files via upload

---
+++ laba 1-2/лаба 1 задание 2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int i, mas[10]{};
+    srand(time(NULL));
+    for (i = 0; i < 10; i++) {
+        mas[i] = rand() % 100; 
+        printf("%d ", mas[i]);
+    }
+    return(0);
+}
