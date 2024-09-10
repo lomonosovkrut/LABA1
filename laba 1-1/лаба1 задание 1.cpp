65bbfdfcfe95e9247a7c9ecfd45d8e05a821d9f4
streltsov <167978599+lomonosovkrut@users.noreply.github.com>
2024-09-10 10:36:09 +0300

    Add files via upload

---
+++ laba 1-1/лаба1 задание 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <locale.h>
+#include <stdio.h>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int arr[6] = { 5, 3, 8, 1, -9, 2 }; // пример массива
+
+    int maxEl = arr[0];
+    int minEl = arr[0];
+
+    for (int i = 1; i < 6; i++) {
+        if (arr[i] > maxEl) {
+            maxEl = arr[i];
+        }
+        if (arr[i] < minEl) {
+            minEl = arr[i];
+        }
+    }
+
+    int difference = maxEl - minEl;
+
+    printf("Максимальный элемент: %d\n", maxEl);
+    printf("Минимальный элемент: %d\n", minEl);
+    printf("Разница: %d\n", difference);
+
+    return 0;
+}
+
